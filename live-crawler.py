5058ce84f72fdb06a575c5ce43b28404c8bb685c
DawonChoi <41173953+DawonChoi@users.noreply.github.com>
2020-10-07 17:32:21 +0900

    commit test2 in Desktop app

--- live-crawler.py
+++ live-crawler.py
@@ -4,7 +4,7 @@ import math
 driver = webdriver.Chrome('C:/discord-bot/util/chromedriver.exe')
 driver.get('http://www.op.gg/')
 
-# commit test in Github Desktop
+# commit test in Github Desktop final
 
 def search(summoner):
     driver.implicitly_wait(3)
